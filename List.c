a6dea317e47f065ed18e58bc5d2236950cc369b8
danielmk <danielkup3@gmail.com>
2021-12-01 19:49:22 +0200

    list create done

--- List.c
+++ List.c
@@ -3,6 +3,7 @@
 //
 
 #include "List.h"
+#include <stdlib.h>
 
 typedef struct node_t{
     ListElement element;
@@ -17,31 +18,17 @@ struct List_t{
     FreeListElement destElement;
 };
 
-/**
-* Allocates a new List.
-*
-* Creates a new empty list. This function receives the functions which will be
-* used for copying elements into the list and freeing them when needed.
-* For example, in order to create a list of strings one need to define these
-* two functions:
-* @code
-* ListElement copyString(ListElement str);
-* void freeString(ListElement str);
-* @endcode
-* And then create a list as follows:
-* @code
-* List listOfStrings = listCreate(copyString, freeString);
-* @endcode
-*
-* @param copyElement Function pointer to be used for copying elements into
-* the list or when copying the list.
-* @param freeElement Function pointer to be used for removing elements from
-* the list.
-* @return
-* 	NULL - if one of the parameters is NULL or allocations failed.
-* 	A new List in case of success.
-*/
-List listCreate(CopyListElement copyElement, FreeListElement freeElement);
+List listCreate(CopyListElement copyElement, FreeListElement freeElement)
+{
+    List new_list= malloc(sizeof(List));
+    if(!new_list)
+    {
+        return NULL;
+    }
+    new_list->cpElement=copyElement;
+    new_list->destElement=freeElement;
+    return new_list;
+}
 
 /**
 * Creates a copy of target list.
@@ -55,7 +42,11 @@ List listCreate(CopyListElement copyElement, FreeListElement freeElement);
 * NULL if a NULL was sent or a memory allocation failed.
 * A List containing the same elements with same order as list otherwise.
 */
-List listCopy(List list);
+List listCopy(List list)
+{
+    List cpList= malloc(sizeof(List));
+
+}
 
 /**
 * Returns the number of elements in a list
