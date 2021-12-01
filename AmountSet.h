a545e5fe57200677eae57af1c9f01cfdda94d260
danielmk <danielkup3@gmail.com>
2021-12-01 16:25:12 +0200

    first commit wish me luck

--- AmountSet.h
+++ AmountSet.h
@@ -1,204 +1,266 @@
-#ifndef SET_H_
-#define SET_H_
+#ifndef AMOUNT_SET_H_
+#define AMOUNT_SET_H_
 
+#include <stdio.h>
 #include <stdbool.h>
 
 /**
-* Generic Set Container
-*
-* Implements a set container type.
-* The set has an internal iterator for external use. For all functions
-* where the state of the iterator after calling that function is not stated,
-* it is undefined. That is you cannot assume anything about it.
-*
-* The following functions are available:
-*   setCreate		- Creates a new empty set
-*   setCopy		- Copies an existing set
-*   setDestroy		- Deletes an existing set and frees all resources
-*   setGetSize		- Returns the size of a given set
-*   setIsIn		- returns weather or not an item exists inside the set.
-*   				  This resets the internal iterator.
-*   setGetFirst	- Sets the internal iterator to the first element in the
-*   				  set, and returns it.
-*   setGetNext		- Advances the internal iterator to the next element and
-*   				  returns it.
-*   setAdd			- Adds a new element to the set.
-*   setRemove		- Removes an element which matches a given element (by the
-*   				  compare function). Resets the internal iterator.
-*	 setClear		- Clears the contents of the set. Frees all the elements of
-*	 				  the set using the free function.
-* 	 SET_FOREACH	- A macro for iterating over the set's elements.
-*/
+ * Generic Amount Set Container
+ *
+ * Implements a sorted amount set container type.
+ * The set has an internal iterator for external use. For all functions
+ * where the state of the iterator after calling that function is not stated,
+ * it is undefined. That is you cannot assume anything about it.
+ * The set is sorted in ascending order - iterating over the set is done in the
+ * same order.
+ *
+ * The following functions are available:
+ *   asCreate           - Creates a new empty set
+ *   asDestroy          - Deletes an existing set and frees all resources
+ *   asCopy             - Copies an existing set
+ *   asGetSize          - Returns the size of the set
+ *   asContains         - Checks if an element exists in the set
+ *   asGetAmount         - Returns the amount of an element in the set
+ *   asRegister         - Add a new element into the set
+ *   asChangeAmount     - Increase or decrease the amount of an element in the set
+ *   asDelete           - Delete an element completely from the set
+ *   asClear            - Deletes all elements from target set
+ *   asGetFirst         - Sets the internal iterator to the first element
+ *                        in the set, and returns it.
+ *   asGetNext          - Advances the internal iterator to the next element
+ *                        and returns it.
+ *   AS_FOREACH         - A macro for iterating over the set's elements
+ */
 
 /** Type for defining the set */
-typedef struct Set_t *Set;
+typedef struct AmountSet_t *AmountSet;
 
-/** Type used for returning error codes from set functions */
-typedef enum SetResult_t {
-    SET_SUCCESS,
-    SET_OUT_OF_MEMORY,
-    SET_NULL_ARGUMENT,
-    SET_ITEM_ALREADY_EXISTS,
-    SET_ITEM_DOES_NOT_EXIST
-} SetResult;
+/** Type used for returning error codes from amount set functions */
+typedef enum AmountSetResult_t {
+    AS_SUCCESS = 0,
+    AS_OUT_OF_MEMORY,
+    AS_NULL_ARGUMENT,
+    AS_ITEM_ALREADY_EXISTS,
+    AS_ITEM_DOES_NOT_EXIST,
+    AS_INSUFFICIENT_AMOUNT
+} AmountSetResult;
 
-/** Element data type for set container */
-typedef void* SetElement;
+/** Element data type for amount set container */
+typedef void *ASElement;
 
 /** Type of function for copying an element of the set */
-typedef SetElement(*copySetElements)(SetElement);
+typedef ASElement (*CopyASElement)(ASElement);
 
 /** Type of function for deallocating an element of the set */
-typedef void(*freeSetElements)(SetElement);
+typedef void (*FreeASElement)(ASElement);
 
 /**
-* Type of function used by the set to identify equal elements.
-* This function will be used to deciding the iteration order of the set.
-* This function should return:
-* 		A positive integer if the first element is greater;
-* 		0 if they're equal;
-*		A negative integer if the second element is greater.
-*/
-typedef int(*compareSetElements)(SetElement, SetElement);
+ * Type of function used by the set to identify equal set elements.
+ * This function should return:
+ *     A positive integer if the first element is greater;
+ *     0 if they're equal;
+ *     A negative integer if the second element is greater.
+ */
+typedef int (*CompareASElements)(ASElement, ASElement);
 
 /**
-* setCreate: Allocates a new empty set.
-*
-* @param copyElement - Function pointer to be used for copying elements into
-*  	the set or when copying the set.
-* @param freeElement - Function pointer to be used for removing elements from
-* 		the set
-* @param compareElements - Function pointer to be used for comparing elements
-* 		inside the set. Used to check if new elements already exist in the set.
-* @return
-* 	NULL - if one of the parameters is NULL or allocations failed.
-* 	A new Set in case of success.
-*/
-Set setCreate(copySetElements copyElement, freeSetElements freeElement, compareSetElements compareElements);
+ * asCreate: Allocates a new empty amount set.
+ *
+ * @param copyElement - Function pointer to be used for copying elements into
+ *     the set or when copying the set.
+ * @param freeElement - Function pointer to be used for removing data elements from
+ *     the set.
+ * @param compareElements - Function pointer to be used for comparing elements
+ *     inside the set. Used to check if new elements already exist in the set.
+ * @return
+ *     NULL - if one of the parameters is NULL or allocations failed.
+ *     A new amount set in case of success.
+ */
+AmountSet asCreate(CopyASElement copyElement,
+                   FreeASElement freeElement,
+                   CompareASElements compareElements);
 
 /**
-* setCopy: Creates a copy of target set.
-*
-* @param set - Target set.
-* @return
-* 	NULL if a NULL was sent or a memory allocation failed.
-* 	A Set containing the same elements as set otherwise.
-*/
-Set setCopy(Set set);
+ * asDestroy: Deallocates an existing amount set. Clears all elements by using
+ * the stored free functions.
+ *
+ * @param set - Target set to be deallocated. If set is NULL nothing will be done.
+ */
+void asDestroy(AmountSet set);
 
 /**
-* setDestroy: Deallocates an existing set. Clears all elements by using the
-* stored free function.
-*
-* @param set - Target set to be deallocated. If set is NULL nothing will be
-* 		done
-*/
-void setDestroy(Set set);
+ * asCopy: Creates a copy of target set.
+ *
+ * Iterator values for both sets are undefined after this operation.
+ *
+ * @param set - Target set.
+ * @return
+ *     NULL if a NULL was sent or a memory allocation failed.
+ *     An amount set containing the same elements (and amounts) as set, otherwise.
+ */
+AmountSet asCopy(AmountSet set);
 
 /**
-* setGetSize: Returns the number of elements in a set
-* @param set - The set which size is requested
-* @return
-* 	-1 if a NULL pointer was sent.
-* 	Otherwise the number of elements in the set.
-*/
-int setGetSize(Set set);
+ * asGetSize: Returns the number of elements in a set.
+ *
+ * Iterator's state is unchanged after this operation.
+ *
+ * @param set - The set which size is requested.
+ * @return
+ *     -1 if a NULL pointer was sent.
+ *     Otherwise the number of elements in the set.
+ */
+int asGetSize(AmountSet set);
 
 /**
-* setIsIn: Checks if an element exists in the set. The element will be
-* considered in the set if one of the elements in the set it determined equal
-* using the comparison function used to initialize the set.
-*
-* @param set - The set to search in
-* @param element - The element to look for. Will be compared using the
-* 		comparison function.
-* @return
-* 	false - if the input set is null, or if the element was not found.
-* 	true - if the element was found in the set.
-*/
-bool setIsIn(Set set, SetElement element);
+ * asContains: Checks if an element exists in the set.
+ *
+ * The element will be considered in the set if one of the elements in the set
+ * it determined equal using the comparison function used to initialize the set.
+ * Iterator's state is unchanged after this operation.
+ *
+ * @param set - The set to search in.
+ * @param element - The element to look for. Will be compared using the
+ *     comparison function.
+ * @return
+ *     false - if the input set is null, or if the element was not found.
+ *     true - if the element was found in the set.
+ */
+bool asContains(AmountSet set, ASElement element);
 
 /**
-*	setGetFirst: Sets the internal iterator (also called current element) to
-*	the first element in the set. The "first" element is the one having the
-*	lowest value as determined by the comparison function used to initialize the
-*	set.
-*	Use this to start iteraing over the set.
-*	Use (To continue iteration use setGetNext)
-*
-* @param set - The set for which to set the iterator and return the first
-* 		element.
-* @return
-* 	NULL if a NULL pointer was sent or the set is empty.
-* 	The first element of the set otherwise
-*/
-SetElement setGetFirst(Set set);
+ * asGetAmount: Returns the amount of an element in the set.
+ *
+ * The function returns an error code indicating wether the operation succeeded,
+ * and in case of success also returns the element's amount via the outAmount
+ * pointer.
+ * Iterator's state is unchanged after this operation.
+ *
+ * @param set - The set which contains the element.
+ * @param element - The element whose amount is requested.
+ * @param outAmount - Pointer to the location where the amount is returned, in case
+ *     of success. In case of failure, the contents of outAmount are unchanged.
+ * @return
+ *     AS_NULL_ARGUMENT - if a NULL argument was passed.
+ *     AS_ITEM_DOES_NOT_EXIST - if the element doesn't exist in the set.
+ *     AS_SUCCESS - if the amount was returned successfully.
+ *
+ * For example, to print the amount of the first item in the set:
+ * @code
+ * double firstAmount;
+ * ASElement first = asGetFirst(set);
+ * if (asGetAmount(set, first, &firstAmount) == AS_SUCCESS) {
+ *     printf("First amount: %f\n", firstAmount);
+ * }
+ * @endcode
+ */
+AmountSetResult asGetAmount(AmountSet set, ASElement element, double *outAmount);
 
 /**
-*	setGetNext: Advances the set iterator to the next element and returns it
-*	The next element is determined by the comparison function induced order.
-* @param set - The set for which to advance the iterator
-* @return
-* 	NULL if reached the end of the set, or the iterator is at an invalid state
-* 	or a NULL sent as argument
-* 	The next element on the set in case of success
-*/
-SetElement setGetNext(Set set);
-
-
-/**
-*	setAdd: Adds a new element to the set.
-*  Iterator's value is undefined after this operation.
-*
-* @param set - The set for which to add an element
-* @param element - The element to insert. A copy of the element will be
-* 		inserted as supplied by the copying function which is given at
-* 		initialization.
-* @return
-* 	SET_NULL_ARGUMENT if a NULL was sent as set
-* 	SET_OUT_OF_MEMORY if an allocation failed (Meaning the function for copying
-* 	an element failed)
-*  SET_ITEM_ALREADY_EXISTS if an equal item already exists in the set
-* 	SET_SUCCESS the element has been inserted successfully
-*/
-SetResult setAdd(Set set, SetElement element);
+ * asRegister: Add a new element into the set.
+ *
+ * The element is added with an initial amount of 0.
+ * Iterator's value is undefined after this operation.
+ *
+ * @param set - The target set to which the element is added.
+ * @param element - The element to add.
+ * @return
+ *     AS_NULL_ARGUMENT - if a NULL argument was passed.
+ *     AS_ITEM_ALREADY_EXISTS - if an equal element already exists in the set.
+ *     AS_SUCCESS - if the element was added successfully.
+ */
+AmountSetResult asRegister(AmountSet set, ASElement element);
 
 /**
-* 	setRemove: Removes an element from the set. The element is found using the
-* 	comparison function given at initialization. Once found, the element is
-* 	removed and deallocated using the free function supplied at initialzation.
-*  Iterator's value is undefined after this operation.
-*
-* @param set -
-* 	The set to remove the element from.
-* @param element
-* 	The element to remove from the set. The element will be freed using the
-* 	free function given at initialization.
-* @return
-* 	SET_NULL_ARGUMENT if a NULL was sent as set
-* 	SET_ITEM_DOES_NOT_EXIST if the element doesn't exist in the set
-* 	SET_SUCCESS if the element was successfully removed.
-*/
-SetResult setRemove(Set set, SetElement element);
+ * asChangeAmount: Increase or decrease the amount of an element in the set.
+ *
+ * Iterator's state is unchanged after this operation.
+ *
+ * @param set - The target set containing the element.
+ * @param element - The element whose amount is increased.
+ * @param amount - How much to change the element's amount. A positive value
+ *     increases element's amount, while a negative value decreases it. A value
+ *     of 0 means don't change it.
+ * @return
+ *     AS_NULL_ARGUMENT - if a NULL argument was passed.
+ *     AS_ITEM_DOES_NOT_EXIST - if the element doesn't exist in the set.
+ *     AS_INSUFFICIENT_AMOUNT - if amount is negative and the element's amount
+ *         in the set is less than the amount that needs to be decreased (i.e.,
+ *         if the change will result in a negative amount for the element in the
+ *         set.)
+ *     AS_SUCCESS - if the element's amount was changed successfully.
+ *
+ * @note parameter amount doesn't affect the return value. Even if amount is 0,
+ *     still AS_ITEM_DOES_NOT_EXIST is returned if element doesn't exist in set,
+ *     and AS_SUCCESS is returned if element exists in set (assuming set is not
+ *     NULL).
+ */
+AmountSetResult asChangeAmount(AmountSet set, ASElement element, const double amount);
 
 /**
-* setClear: Removes all elements from target set.
-* The elements are deallocated using the stored free function
-* @param set
-* 	Target set to remove all element from
-* @return
-* 	SET_NULL_ARGUMENT - if a NULL pointer was sent.
-* 	SET_SUCCESS - Otherwise.
-*/
-SetResult setClear(Set);
+ * asDelete: Delete an element completely from the set.
+ *
+ * This is the opposite of asRegister. The element is deallocated using the
+ * stored free function.
+ * Iterator's value is undefined after this operation.
+ *
+ * @param set - The target set from which the element is deleted.
+ * @param element - The element to delete.
+ * @return
+ *     AS_NULL_ARGUMENT - if a NULL argument was passed.
+ *     AS_ITEM_DOES_NOT_EXIST - if the element doesn't exist in the set.
+ *     AS_SUCCESS - if the element was deleted successfully.
+ */
+AmountSetResult asDelete(AmountSet set, ASElement element);
 
-/*!
-* Macro for iterating over a set.
-* Declares a new iterator for the loop.
-*/
-#define SET_FOREACH(type,iterator,set) \
-	for(type iterator = setGetFirst(set) ; \
-		iterator ;\
-		iterator = setGetNext(set))
+/**
+ * asClear: Deletes all elements from target set.
+ *
+ * The elements are deallocated using the stored free function.
+ * Iterator's value is undefined after this operation.
+ *
+ * @param set - Target set to delete all elements from.
+ * @return
+ *     AS_NULL_ARGUMENT - if a NULL pointer was sent.
+ *     AS_SUCCESS - Otherwise.
+ */
+AmountSetResult asClear(AmountSet set);
 
-#endif /* SET_H_ */
+/**
+ * asGetFirst: Sets the internal iterator (also called current element) to
+ * the first  element in the set. The first element is the smallest element
+ * of the set, according to the set's comparison function.
+ * Use this to start iterating over the set.
+ * To continue iteration use asGetNext.
+ *
+ * @param set - The set for which to set the iterator and return the first
+ *     element.
+ * @return
+ *     NULL if a NULL pointer was sent or the set is empty.
+ *     The first element of the set otherwise
+ */
+ASElement asGetFirst(AmountSet set);
+
+/**
+ * asGetNext: Advances the set iterator to the next element and returns it.
+ * The iteration is in ascending order on the set's elements, according to the
+ * set's comparison function.
+ *
+ * @param set - The set for which to advance the iterator
+ * @return
+ *     NULL if reached the end of the set, or the iterator is at an invalid state
+ *     or a NULL sent as argument
+ *     The next element on the set in case of success
+ */
+ASElement asGetNext(AmountSet set);
+
+/**
+ * Macro for iterating over a set.
+ * Declares a new iterator for the loop.
+ */
+#define AS_FOREACH(type, iterator, set)          \
+    for(type iterator = (type) asGetFirst(set) ; \
+        iterator ;                               \
+        iterator = asGetNext(set))
+
+#endif /* AMOUNT_SET_H_ */
