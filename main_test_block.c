099e7373a76b6ddc32f3a9e4844da6af3e990bfd
danielmk <danielkup3@gmail.com>
2021-12-02 22:17:13 +0200

    ver 11

--- main_test_block.c
+++ main_test_block.c
@@ -18,7 +18,15 @@ int main()
     List test= listCreate(intCopy,intDest);
     if(!test)
     {
-        printf("test 1 failed");
+        printf("test 1 failed\n");
+        return 0;
+    }
+    int tmp= 7;
+    int* ptmp= &tmp;
+    if(listInsertFirst(test,ptmp)==LIST_SUCCESS)
+    {
+        printf("failed at test 2\n");
+        listDestroy(test);
         return 0;
     }
 
