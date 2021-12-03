ad2cf29503a6705681fe55ac9935ddff51a456c9
danielmk <danielkup3@gmail.com>
2021-12-03 08:47:43 +0200

    ver 12

--- main_test_block.c
+++ main_test_block.c
@@ -13,6 +13,10 @@ void intDest(void* org)
     free((int*)org);
 }
 
+int intCompare(void* first, void* second)
+{
+    return *(int*)first-*(int*)second;
+}
 int main()
 {
     List test= listCreate(intCopy,intDest);
@@ -21,11 +25,22 @@ int main()
         printf("test 1 failed\n");
         return 0;
     }
-    int tmp= 7;
-    int* ptmp= &tmp;
-    if(listInsertFirst(test,ptmp)==LIST_SUCCESS)
+    for (int i = 0; i < 100; ++i)
     {
-        printf("failed at test 2\n");
+        int tmp= i;
+        int* ptmp= &tmp;
+        if(listInsertFirst(test,ptmp)==LIST_SUCCESS)
+        {
+            printf("failed at test 2\n");
+            listDestroy(test);
+            return 0;
+        }
+        tmp=i+1;
+        if (listInsertLast(test,ptmp));
+    }
+    if(listSort(test,intCompare)!=LIST_SUCCESS)
+    {
+        printf("failed at test 3\n");
         listDestroy(test);
         return 0;
     }
