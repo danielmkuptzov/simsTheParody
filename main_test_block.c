99fe9956c2a33babe2a34ab6729b92a20e360560
danielmk <danielkup3@gmail.com>
2021-12-24 18:52:35 +0200

    ver 45: before testing

--- main_test_block.c
+++ main_test_block.c
@@ -55,72 +55,81 @@ int main()
         if(*elemTest!=testarr[i])
         {
             printf("whyyyyyyyyyyyyyyyyyyyyy");
-            listDestroy(test);
+            setDestroy(test);
         }
         i++;
     }
-    if(listGetSize(test)!=10)
+    int j=5;
+    if(setAdd(test,&j)!=SET_ITEM_ALREADY_EXISTS)
     {
-        printf("test 3 failed\n");
-        listDestroy(test);
+        printf("noooooooooooooooo");
+        setDestroy(test);
         return 0;
     }
-    if(listSort(test,intCompare)!=LIST_SUCCESS)
+    if(setGetSize(test)!=10)
+    {
+        printf("test 3 failed\n");
+        setDestroy(test);
+        return 0;
+    }
+    if(setOrder(test)!=SET_SUCCESS)
     {
         printf("failed at test 4\n");
-        listDestroy(test);
+        setDestroy(test);
         return 0;
     }
     i=0;
-    LIST_FOREACH(int*,order,test)
+    SET_FOREACH(int*,order,test)
     {
         if(*order!=(i+1))
         {
-            listDestroy(test);
+            setDestroy(test);
             return 0;
         }
         i++;
     }
-    List liscp= listCopy(test);
+    Set liscp= setCopy(test);
     if(!liscp)
     {
         printf("test 8 failed\n");
-        listDestroy(test);
+        setDestroy(test);
         return 0;
     }
-    LIST_FOREACH(int*,iter,test)
+    int k=9;
+    SET_FOREACH(int*,iter,test)
     {
-        if(*iter==9)
+        if(*iter==k)
         {
-            if(listRemoveCurrent(test)!=LIST_SUCCESS)
+            if(setRemove(test,&k)!=SET_SUCCESS)
             {
                 printf("fail");
-                listDestroy(test);
+                setDestroy(test);
+                return 0;
             }
         }
     }
-    if(listClear(test)!=LIST_SUCCESS)
+    if(setClear(test)!=SET_SUCCESS)
     {
         printf("test 7 failed\n");
-        listDestroy(test);
+        setDestroy(test);
         return 0;
     }
-    listDestroy(test);
+    setDestroy(test);
     int key=5;
-    List filtered= listFilter(liscp,intFilter,&key);
+    Set filtered= setFilter(liscp,intFilter,&key);
     if(!filtered)
     {
         printf("test 9 failed\n");
-        listDestroy(liscp);
+        setDestroy(liscp);
         return 0;
     }
-    listDestroy(liscp);
-    if(listClear(filtered)!=LIST_SUCCESS)
+    setDestroy(liscp);
+    if(setClear(filtered)!=SET_SUCCESS)
     {
         printf("final test failed\n");
-        listDestroy(filtered);
+        setDestroy(filtered);
         return 0;
     }
-    listDestroy(filtered);
+    setDestroy(filtered);
     return 0;
 }
