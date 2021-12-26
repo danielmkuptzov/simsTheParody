a7c44fd3879627757f9bd4d4a9e238c293b2f658
danielmk <danielkup3@gmail.com>
2021-12-26 09:57:22 +0200

    ver 61: product added

---
+++ Product.c
@@ -0,0 +1,5 @@
+//
+// Created by danie on 26/12/2021.
+//
+
+#include "Product.h"
---
+++ Product.h
@@ -0,0 +1,32 @@
+//
+// Created by danie on 26/12/2021.
+//
+
+#ifndef DANIELCITY_PRODUCT_H
+#define DANIELCITY_PRODUCT_H
+
+/**
+ * Product adt
+ *
+ * Implements a product variable type.
+ * our product would be sorted by the id given by the user.
+ * The names would be checked by the string standart.
+ *
+ * @important be shure what type of product you want. we wonwt tolerate expiriment of changing to wrong type.
+ *
+ *
+ * The following functions are available:
+ *   productCreate            - Creates a new product
+ *   productDestroy           - Deletes an existing product and frees all resources
+ *   productCopy              - Copies an existing product
+ *   productEquals            -checks if the products are the same
+ *   productGetId             -for serching purposes
+ *   productGetType           -gives the type of the product
+ *   productGetComponent      -givesThe components of the product
+ *   productAddComponent      -for adding to components
+ *   productRemoveComponent   -for removing component
+ *   productGetAdditionalData -
+ */
+
+
+#endif //DANIELCITY_PRODUCT_H
