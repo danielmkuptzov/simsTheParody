169c23061d9cc1d3f83899b1ea58647fcc520d04
danielmk <danielkup3@gmail.com>
2021-12-31 11:37:55 +0200

    kernel ver 20: only to press the green buttom

--- main_test_block.c
+++ main_test_block.c
@@ -22,15 +22,30 @@ int intCompare(void* first, void* second)
     int* two=second;
     return one-two;
 }
-bool intFilter(void* data, void* limit)
+void* rasCopy(void* org)
 {
-    int* element=data;
-    int* key=limit;
-    if((*element)<(*key))
+    return rationalCopy((Rational)org);
+}
+void rasDest(void* org)
+{
+    rationalDestroy((Rational)org);
+}
+int rasCompare(void* first, void* second)
+{
+    return rationalCompare((Rational)first,(Rational)second);
+}
+bool greaterThanEpsilon(void* ras, void* key)
+{
+    Rational epsilon= rationalCreate(0,1);
+    Rational diff= rationalAddition(epsilon,(Rational)ras);
+    bool resalt=false;
+    if(rationalEqual((Rational)key,diff))
     {
-        return true;
+        resalt=true;
     }
-    return false;
+    rationalDestroy(epsilon);
+    rationalDestroy(diff);
+    return resalt;
 }
 void intAdvance(void* org)
 {
@@ -53,32 +68,37 @@ int main()
     CoreUnit tmp= coreCreate(2,NULL,NULL,NULL,-1);
     Product product= productCreate(1,"apple",HALF_INTEGER_AMOUNT,
                                    intCopy,intDest, coreGetElement(tmp),
-                                   coreCopy,coreDestroy,coreCompeare,&j,2);
+                                   rasCopy,rasDest,rasCompare,&j,3);
     coreDestroy(tmp);
     for(int i=0; i<10; i++)
     {
-        CoreUnit unit=coreCreate(2,NULL,NULL,NULL,1);
-        if(productAddComponent(product,unit)!=PRODUCT_SUCSESS)
+        Rational temp= rationalCreate(1,i+1);
+        if(productAddComponent(product,temp)!=PRODUCT_SUCSESS)
         {
             return 0;
         }
-        coreDestroy(unit);
+        rationalDestroy(temp);
     }
     Product product2= productCopy(product);
     for(int i=0; i<10; i++)
     {
-        CoreUnit unit= coreCreate(2, NULL,NULL,NULL,-1);
-        if(productAddComponent(product2,unit)!=PRODUCT_SUCSESS)
+        Rational temp= rationalCreate(2,i+1);
+        if(productAddComponent(product2,temp)!=PRODUCT_SUCSESS)
         {
             return 0;
         }
-        coreDestroy(unit);
+        rationalDestroy(temp);
     }
     Product product3= productUnite(product,product2);
     if(!product3)
     {
         printf("bad");
     }
+    productDestroy(product);
+    Rational k= rationalCreate(1,10);
+    CoreUnit toFilter= coreCreate(1,NULL,NULL,NULL,0);
+    if(coreSetElement(toFilter, productComponentFilter(product3,greaterThanEpsilon,k),1));
+    rationalDestroy(k);
     productDestroy(product);
     productDestroy(product2);
     productDestroy(product3);
