e69edf842ac55055763a02bb719e8c9a932b2342
danielmk <danielkup3@gmail.com>
2021-12-31 15:29:49 +0200

    kernel ver 24: header documentation

--- OrderProduct.h
+++ OrderProduct.h
@@ -1,8 +1,17 @@
-//
-// Created by danie on 31/12/2021.
-//
-
 #ifndef DANIELCITY_ORDERPRODUCT_H
 #define DANIELCITY_ORDERPRODUCT_H
+
+/**
+ *  Rational numbers adt
+ *
+ * this header implements the rational numbers. we will recive in the creator only integers (no doubles)
+ * please use with care
+ *
+ * The following functions are available:
+ *   rationalCreate         -Creates the rational number.
+ *   rationalDestroy        -Deletes an existing rational and frees all resources
+ *   rationalCopy           -Copies an existing rational
+ *   rationalCompeare       -compares between rationals
+ */
 
 #endif //DANIELCITY_ORDERPRODUCT_H
