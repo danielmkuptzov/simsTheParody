5294b984405ea5117db6f095d4b33310d5811b5d
danielmk <danielkup3@gmail.com>
2022-03-08 13:15:04 +0200

    street core ver 1: finaly

---
+++ Apartment.c
@@ -0,0 +1,5 @@
+//
+// Created by danie on 08/03/2022.
+//
+
+#include "Apartment.h"
---
+++ Apartment.h
@@ -0,0 +1,23 @@
+//
+// Created by danie on 08/03/2022.
+//
+
+#ifndef DANIELCITY_APARTMENT_H
+#define DANIELCITY_APARTMENT_H
+
+/**
+ *  adt
+ *
+ * Implements a
+ *
+ *
+ *
+ * The following functions are available:
+ *   Create            - Creates a new
+ *   Destroy           - Deletes an existing and frees all resources
+ *   Copy              - Copies an existing
+ *   Compeare          -compares between
+ */
+
+
+#endif //DANIELCITY_APARTMENT_H
