0e9f353a818a20dfeaceb35359f2beee24604d40
danielmk <danielkup3@gmail.com>
2023-01-10 06:41:31 +0200

    testing ver 180

--- Kernel/outerCore/outerCore.c
+++ Kernel/outerCore/outerCore.c
@@ -12,10 +12,10 @@ struct CoreUnit_t{
 void coreBeginner(CopyRefDate copyFunc, FreeRefDate freeFunc,
                   RefDateAdvance advanceFunc,DifferenceCalculator diffFunc,
                   ReferanceDate date, DayOne firstDay,YearComp ctour,YearDestroy dtour,YearZero zero,
-                  )
+                  Yearlowestposs lowest,YearAdvance advance,MaximalYear max,)
 {
     dateInitialiser(copyFunc,freeFunc,advanceFunc,diffFunc,date,firstDay,ctour,dtour,zero,
-                    lowest,copyYear,destYear,yearzero,);
+                    lowest,advance,max,);
 }
 
 
