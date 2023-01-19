7eac8c96df4fa2b4e276cacc0ee1c0aac3168569
danielmk <danielkup3@gmail.com>
2023-01-19 07:28:31 +0200

    testing ver 187

--- Kernel/Kernel.c
+++ Kernel/Kernel.c
@@ -128,7 +128,8 @@ void kernelBeginner(CopyExternal copyFunc, DestExternal freeFunc,
                     Yearlowestposs lowest,YearAdvance advance,MaximalYear max,YearComp compeare,
                     Yearinit init, Yearbackadvance backadv, YearDivide div)
 {
-    coreBeginner(copyFunc,freeFunc,advanceFunc,diffFunc,date,firstDay,ctour,dtour,zero,);
+    coreBeginner(copyFunc,freeFunc,advanceFunc,diffFunc,date,firstDay,ctour,dtour,zero,lowest,
+                 advance,);
 }
 
 void kernelEpsilonSetter(int base)
