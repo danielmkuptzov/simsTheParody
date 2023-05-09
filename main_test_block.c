41652f2f627161ae064f038a1939f070fae37dcb
danielmk <danielkup3@gmail.com>
2023-05-09 06:11:26 +0300

    testing ver 265

--- main_test_block.c
+++ main_test_block.c
@@ -370,7 +370,7 @@ int main(void)
         return 0;
     }
     //find use for Person apartmentGetOwner(Apartment apartment); but stil dont know how, maybe to  just test
-    Kernel date= kernelCreate(DATE,true,);
+    Kernel date= kernelCreate(DATE,true,NULL,0);
     int* idq;
     id=7;
     idq=&id;
