7af5140cd154a718bf1d8530aa85c529765ef97c
danielmk <danielkup3@gmail.com>
2023-09-18 06:40:22 +0300

    testing ver 376

--- Apartment.c
+++ Apartment.c
@@ -727,7 +727,7 @@ ApartmentErrorCodes apartmentDayCycle(Apartment apartment, ApartmentCycleErrorCo
         ApartmentErrorCodes resalt=APARTMENT_SUCSESS;
         KERNEL_FOREACH(Kernel,iter,object)
         {
-            //conversion problem? intresting why? realy yep ? conversion prob? yep? why
+            //conversion problem? intresting why? realy yep ? conversion prob? yep? why? realy
 
             if(kernelGetType(iter)==FURNITURE)
             {
