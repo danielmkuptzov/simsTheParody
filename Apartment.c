0e7207e9f3669b23648051b54821f9a3a86f3d62
danielmk <danielkup3@gmail.com>
2023-09-25 06:56:51 +0300

    testing ver 382

--- Apartment.c
+++ Apartment.c
@@ -727,7 +727,7 @@ ApartmentErrorCodes apartmentDayCycle(Apartment apartment, ApartmentCycleErrorCo
         ApartmentErrorCodes resalt=APARTMENT_SUCSESS;
         KERNEL_FOREACH(Kernel,iter,object)
         {
-            //conversion problem? try to find anew type? strange? why prob??
+            //conversion problem? try to find anew type? strange? why prob???
 
             if(kernelGetType(iter)==FURNITURE)
             {
