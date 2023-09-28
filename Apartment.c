aab69deb5be46c59f6edba14bfa4bbaf2d3e241e
danielmk <danielkup3@gmail.com>
2023-09-28 06:47:32 +0300

    testing ver 385

--- Apartment.c
+++ Apartment.c
@@ -729,7 +729,7 @@ ApartmentErrorCodes apartmentDayCycle(Apartment apartment, ApartmentCycleErrorCo
         {
             //conversion problem? try to find anew type? strange? why prob??? why??
 
-            if(kernelGetType(iter)==FURNITURE)
+            if(kernelGetType(iter)==FURNITURE)//strange
             {
                 resalt=apartmentAddFurniture(apartment,iter);
             }
