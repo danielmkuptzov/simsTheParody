2db40c012f2d9052f5d7fb9f5fab73b28229e9a4
danielmk <danielkup3@gmail.com>
2023-11-01 07:37:28 +0200

    testing ver 413

--- Apartment.c
+++ Apartment.c
@@ -727,7 +727,7 @@ ApartmentErrorCodes apartmentDayCycle(Apartment apartment, ApartmentCycleErrorCo
         ApartmentErrorCodes resalt=APARTMENT_SUCSESS;
         KERNEL_FOREACH(Kernel,iter,object)
         {
-            //strange. test. not know how. sure?? dont know. surley?????
+            //strange. test. not know how. sure?? dont know. surley?????????
             if(kernelGetType(iter)==FURNITURE)//strange, yep why? realy???
             {
                 resalt=apartmentAddFurniture(apartment,iter);
