f284c5f893d4d80d0c86c33a0d6f61dd66b4b545
danielmk <danielkup3@gmail.com>
2023-12-12 09:45:40 +0200

    changes ver 21

--- Kernel.h
+++ Kernel.h
@@ -194,7 +194,7 @@ int  stringComp(char* first, char* second);
  * @param date
  * @param firstDay
  */
- //to check the code for duplicates of defs ok not sure realy?
+ //to check the code for duplicates of defs ok not sure realy? yep
 void kernelBeginner(CopyExternal copyFunc, DestExternal freeFunc,
                     AdvanceExternal advanceFunc,DifferenceForCync diffFunc,
                     ExternalDate date, CyncFunc firstDay,YearCp ctour,YearDest dtour,YearZeroexter zero,
