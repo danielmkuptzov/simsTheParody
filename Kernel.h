2b141e3cb636e0da80e46285b6ffe344c8db773e
danielmk <danielkup3@gmail.com>
2023-12-27 07:46:22 +0200

    changes ver 32

--- Kernel.h
+++ Kernel.h
@@ -194,7 +194,7 @@ int  stringComp(char* first, char* second);
  * @param date
  * @param firstDay
  */
- //conventions in whitch way realy? hahahh to think about it intresting
+ //why
 void kernelBeginner(CopyExternal copyFunc, DestExternal freeFunc,
                     AdvanceExternal advanceFunc,DifferenceForCync diffFunc,
                     ExternalDate date, CyncFunc firstDay,YearCp ctour,YearDest dtour,YearZeroexter zero,
