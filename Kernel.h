42589ddec4fa663b246fd49c63f20fb0c5402e55
danielmk <danielkup3@gmail.com>
2024-01-20 12:42:04 +0200

    changes ver 48

--- Kernel.h
+++ Kernel.h
@@ -194,7 +194,7 @@ int  stringComp(char* first, char* second);
  * @param date
  * @param firstDay
  */
- //to edit work on it
+ //to edit work on it????
 void kernelBeginner(CopyExternal copyFunc, DestExternal freeFunc,
                     AdvanceExternal advanceFunc,DifferenceForCync diffFunc,
                     ExternalDate date, CyncFunc firstDay,YearCp ctour,YearDest dtour,YearZeroexter zero,
