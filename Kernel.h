04b7687a5664d75dd80587d4601cddd593f0fffc
danielmk <danielkup3@gmail.com>
2024-01-28 07:28:13 +0200

    changes ver 55

--- Kernel.h
+++ Kernel.h
@@ -194,7 +194,7 @@ int  stringComp(char* first, char* second);
  * @param date
  * @param firstDay
  */
- //simple name change????? realy???? ??????? why
+ //simple name change????? realy???? ??????? why???????
 void kernelBeginner(CopyExternal copyFunc, DestExternal freeFunc,
                     AdvanceExternal advanceFunc,DifferenceForCync diffFunc,
                     ExternalDate date, CyncFunc firstDay,YearCp ctour,YearDest dtour,YearZeroexter zero,
