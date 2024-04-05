c7f3fd92ed2d373443a649eb90a2ca1219060371
danielmk <danielkup3@gmail.com>
2024-04-05 07:46:54 +0300

    changes ver 113

--- Kernel.h
+++ Kernel.h
@@ -194,7 +194,7 @@ int  stringComp(char* first, char* second);
  * @param date
  * @param firstDay
  */
- //dont forget to write why? why intresting why? realy? why? strange behaviour why???? why????
+ //dont forget to write why? why intresting why? realy?
 void kernelBeginner(CopyExternal copyFunc, DestExternal freeFunc,
                     AdvanceExternal advanceFunc,DifferenceForCync diffFunc,
                     ExternalDate date, CyncFunc firstDay,YearCp ctour,YearDest dtour,YearZeroexter zero,
