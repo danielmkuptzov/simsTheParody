feb27fc63f2705c63aa25f16577afa857b63b420
danielmk <danielkup3@gmail.com>
2024-04-25 07:00:23 +0300

    changes ver 133

--- Kernel.h
+++ Kernel.h
@@ -194,7 +194,7 @@ int  stringComp(char* first, char* second);
  * @param date
  * @param firstDay
  */
- //dont forget to write why? cool to work on it why intresting why
+ //dont forget to write why? cool to work on it why intresting why?????
 void kernelBeginner(CopyExternal copyFunc, DestExternal freeFunc,
                     AdvanceExternal advanceFunc,DifferenceForCync diffFunc,
                     ExternalDate date, CyncFunc firstDay,YearCp ctour,YearDest dtour,YearZeroexter zero,
