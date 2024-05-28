91fe9ba833b8b69a829403fed909c4aee1c600c9
danielmk <danielkup3@gmail.com>
2024-05-28 08:25:55 +0300

    changes ver 163

--- Kernel.h
+++ Kernel.h
@@ -194,7 +194,7 @@ int  stringComp(char* first, char* second);
  * @param date
  * @param firstDay
  */
- //dont forget to write what why? worthy or not we will see today yes
+ //dont forget to write what why? worthy or not we will see today yes ealy?
 void kernelBeginner(CopyExternal copyFunc, DestExternal freeFunc,
                     AdvanceExternal advanceFunc,DifferenceForCync diffFunc,
                     ExternalDate date, CyncFunc firstDay,YearCp ctour,YearDest dtour,YearZeroexter zero,
