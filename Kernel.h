b4f58b2f5cfbb125be55df51818a28f2e40fdbed
danielmk <danielkup3@gmail.com>
2024-06-28 08:04:29 +0300

    changes ver 192

--- Kernel.h
+++ Kernel.h
@@ -194,7 +194,7 @@ int  stringComp(char* first, char* second);
  * @param date
  * @param firstDay
  */
- //dont forget to write what why? bad cable managment, intresting strange
+ //dont forget to write what why? bad cable managment, intresting strange, raly strange
 void kernelBeginner(CopyExternal copyFunc, DestExternal freeFunc,
                     AdvanceExternal advanceFunc,DifferenceForCync diffFunc,
                     ExternalDate date, CyncFunc firstDay,YearCp ctour,YearDest dtour,YearZeroexter zero,
