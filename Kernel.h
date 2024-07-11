35e16fd25f102d32db653f14dbe2233758e76255
danielmk <danielkup3@gmail.com>
2024-07-11 06:23:05 +0300

    changes ver 205

--- Kernel.h
+++ Kernel.h
@@ -194,7 +194,7 @@ int  stringComp(char* first, char* second);
  * @param date
  * @param firstDay
  */
- //dont forget to write what why? vcs to wet 1 nachs
+ //dont forget to write what why? did it
 void kernelBeginner(CopyExternal copyFunc, DestExternal freeFunc,
                     AdvanceExternal advanceFunc,DifferenceForCync diffFunc,
                     ExternalDate date, CyncFunc firstDay,YearCp ctour,YearDest dtour,YearZeroexter zero,
