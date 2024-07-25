90197a785296c7d208408e56e542f33238ba5c56
danielmk <danielkup3@gmail.com>
2024-07-25 14:59:51 +0300

    changes ver 218

--- Kernel.h
+++ Kernel.h
@@ -194,7 +194,7 @@ int  stringComp(char* first, char* second);
  * @param date
  * @param firstDay
  */
- //dont forget to write what why? did it think about work why network test why intresting
+ //dont forget to write what why? did it think about work why network test why intresting strange
 void kernelBeginner(CopyExternal copyFunc, DestExternal freeFunc,
                     AdvanceExternal advanceFunc,DifferenceForCync diffFunc,
                     ExternalDate date, CyncFunc firstDay,YearCp ctour,YearDest dtour,YearZeroexter zero,
