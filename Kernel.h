32b190fab5f7c3245a249701d2f513d5d5e8a233
danielmk <danielkup3@gmail.com>
2024-07-31 08:48:11 +0300

    changes ver 224

--- Kernel.h
+++ Kernel.h
@@ -194,7 +194,7 @@ int  stringComp(char* first, char* second);
  * @param date
  * @param firstDay
  */
- //dont forget to write what why? did it think about work why network test why ok mri hahha why
+ //dont forget to write what why? is revenge good or bad?
 void kernelBeginner(CopyExternal copyFunc, DestExternal freeFunc,
                     AdvanceExternal advanceFunc,DifferenceForCync diffFunc,
                     ExternalDate date, CyncFunc firstDay,YearCp ctour,YearDest dtour,YearZeroexter zero,
