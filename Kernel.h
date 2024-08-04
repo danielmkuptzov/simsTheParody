0d3c623557f95174b3bf53dd8ba4653c3500b9b1
danielmk <danielkup3@gmail.com>
2024-08-04 11:43:28 +0300

    changes ver 228

--- Kernel.h
+++ Kernel.h
@@ -194,7 +194,7 @@ int  stringComp(char* first, char* second);
  * @param date
  * @param firstDay
  */
- //dont forget to write what why? strange no cool to hold push like that why
+ //dont forget to write what why? strange no cool to hold
 void kernelBeginner(CopyExternal copyFunc, DestExternal freeFunc,
                     AdvanceExternal advanceFunc,DifferenceForCync diffFunc,
                     ExternalDate date, CyncFunc firstDay,YearCp ctour,YearDest dtour,YearZeroexter zero,
