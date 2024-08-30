4d601769fc3156e76f5af609f581281b8a733088
danielmk <danielkup3@gmail.com>
2024-08-30 14:33:32 +0300

    changes ver 253

--- Kernel.h
+++ Kernel.h
@@ -194,7 +194,7 @@ int  stringComp(char* first, char* second);
  * @param date
  * @param firstDay
  */
- //dont forget to write constants, yep intresting why intresting and good luck to me i have test tomorow
+ //dont forget to write constants, yep intresting why
 void kernelBeginner(CopyExternal copyFunc, DestExternal freeFunc,
                     AdvanceExternal advanceFunc,DifferenceForCync diffFunc,
                     ExternalDate date, CyncFunc firstDay,YearCp ctour,YearDest dtour,YearZeroexter zero,
