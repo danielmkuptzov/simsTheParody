b9a844562e800ce8191080f445338bc07a066d11
danielmk <danielkup3@gmail.com>
2024-09-16 08:25:46 +0300

    changes ver 269

--- Kernel.h
+++ Kernel.h
@@ -194,7 +194,7 @@ int  stringComp(char* first, char* second);
  * @param date
  * @param firstDay
  */
- //dont forget to write constants, who knows why realy??? catch 22 situation bloody hell
+ //dont forget to write constants, who knows why realy??? a difficult choise
 void kernelBeginner(CopyExternal copyFunc, DestExternal freeFunc,
                     AdvanceExternal advanceFunc,DifferenceForCync diffFunc,
                     ExternalDate date, CyncFunc firstDay,YearCp ctour,YearDest dtour,YearZeroexter zero,
